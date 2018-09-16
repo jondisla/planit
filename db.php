4ca6f494c267829555fd76e651ecd6362d9d538c
Jon Disla <jond@Jons-Mac-mini.local>
2018-09-15 22:20:21 -0400

    deploy commit

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "planit";
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$username = "b186ee9f845e68";
+$password = "4b9feda0";
+$dbname = "heroku_5127e81f159d979";
 
     $connection = new mysqli($servername, $username, $password, $dbname);
     
